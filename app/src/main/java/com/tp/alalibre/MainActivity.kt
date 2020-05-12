f3b1235ed0a7039817bbf6ddc0607ce7b1450f04
ailenaguino <ailenaguino@gmail.com>
2020-05-12 01:13:54 -0300

    Toolbar Layout, detalle de producto layout, agregar producto layout

--- app/src/main/java/com/tp/alalibre/MainActivity.kt
+++ app/src/main/java/com/tp/alalibre/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.tp.alalibre
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnIrDetalleProducto.setOnClickListener{
+            startActivity(Intent(this, DetalleProducto::class.java))
+        }
+        btnirAgregarProducto.setOnClickListener{
+            startActivity(Intent(this, AgregarProducto::class.java))
+        }
+    }
+
+    override fun onStart(){
+        super.onStart()
     }
 }
